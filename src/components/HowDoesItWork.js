92bba5f2b684a68c6187f20973e49caa2a60a9e2
Refael <refaelbenzvi24@gmail.com>
2022-06-29 21:08:44 +0300

    added a progress bar - installed nprogress  - implemented as a plugin which will be triggered on every route change to display route loading - added react-spinners-kit  - temporarily implemented it in "/how-does-it-work" route for later use convenience

---
+++ src/components/HowDoesItWork.js
@@ -0,0 +1,12 @@
+import ProgressSpinner from "./UI/Progress/ProgressSpinner";
+
+
+const HowDoesItWork = () => {
+    return (
+        <div className="centered-div">
+            <ProgressSpinner/>
+        </div>
+    )
+}
+
+export default HowDoesItWork
