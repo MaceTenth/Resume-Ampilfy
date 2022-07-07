2867fa8b1706314cffd2e1a5ed7c2644da5b8080
Refael <refaelbenzvi24@gmail.com>
2022-07-07 13:22:20 +0300

    removed routes.js file - a different approach to isolate react-router context provider from the routes  - moved the provider into the render function call

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import {render} from "react-dom";
 import "./style/index.css";
-import Routes from "./routes";
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import ResumePaste from "./components/ResumePaste";
+import HowDoesItWork from "./components/HowDoesItWork";
+import Contact from "./components/Contact";
+import initializePlugins from "./plugins";
 
 function App() {
+    initializePlugins()
+
     return (
         <div className="App">
             <h1>Resume Amplify</h1>
-            <Router>
-                <Routes/>
-            </Router>
+            <Navbar/>
+            <Switch>
+                <Route path="/" exact>
+                    <div className="container">
+                        <About/>
+                        <ResumePaste/>
+                    </div>
+                </Route>
+                <Route path="/how-does-it-work">
+                    <HowDoesItWork/>
+                </Route>
+                <Route path="/Contact">
+                    <Contact/>
+                </Route>
+            </Switch>
         </div>
     )
 }
 
 render(
-    <App/>,
+    <Router>
+        <App/>
+    </Router>,
     document.getElementById("root")
-);
+)
